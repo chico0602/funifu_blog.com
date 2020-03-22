cbc54dec80fd4ba9ef73363dfd203c61a76be3c5
chico <yukamori.0602@gmail.com>
2020-03-22 14:16:48 +0900

    update

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import pymysql
+pymysql.install_as_MySQLdb()
+pymysql.version_info = 1, 4, 2, 'final', 0
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,10 +79,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydjango',
+        'USER': 'mydjango',
+        'PASSWORD': 'mydjangopasswd',
+        'HOST': '10.0.2.10',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
